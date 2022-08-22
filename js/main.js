e6174ffd5405815b2db254455c4dd730d8ec17e3
Tales de Melo <100959219+talesco098@users.noreply.github.com>
2022-08-21 22:36:52 -0300

    funções

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll(".controle-ajuste")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
+controle.forEach((elemento) =>{
+    elemento.addEventListener("click", (evento) =>{
+        manipulaDados(evento.target.textContent)
+    })
 })
 
-function dizOi (nome){
-console.log("Oi" + nome)
-console.log("Bem vindo ao Robotron 2000")
-}
-
-dizOi("Tales")
+  function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+       braco.value = parseInt(braco.value) + 1 
+    }
+  }
